6cd3f005030fa008197580284a91afbe3bcbdafb
Dev Khullar <d.khullar@uleth.ca>
2025-07-04 08:53:59 -0600

    Add resources

---
+++ README.md
@@ -0,0 +1,9 @@
+# DAY in the LIFE of an Astronomer
+
+This workshop is a part of the programme InnovateHER held at the University of Lethbridge, Lethbridge, Alberta, Canada. In this workshop, students aged 9-12 years are going to be given a very basic introduction to tools used by professional astronomers for their research, particularly using DS9 for astronomical imaging and using Python for point source identification in an astronomical image (see [notebook](notebook link)).
+
+The students will be provided with laptops with DS9 preinstalled and will use the image of the galaxy M66 (NGC 3627) from the James Webb Space Telescope to initiate discussions on various astronomical concepts, from supermassive blackholes, stars, to extraterrestrial life. 
+
+Then, they will be given a brief demonstration of how I employ point source identification for my research. For the sake of understanding, a lot of the code has been set to default so that more emphasis can be put on the why's and the how's in astronomy rather than the code explanation. 
+
+To access the resources on the cloud, please use the binder link above which will open the resources in a new window where you can use the code interactively.
--- starfind.py
+++ starfind.py
@@ -7,43 +7,167 @@ from photutils.detection import DAOStarFinder
 from astropy.visualization import SqrtStretch
 from astropy.visualization.mpl_normalize import ImageNormalize
 from photutils.aperture import CircularAperture
+from photutils.datasets import load_star_image
+from XRBID.WriteScript import WriteReg
+from astropy.wcs import WCS
 
-def show(image, vmin=0, vmax=10, colourmap='viridis', figsize=(10, 8)):
-    '''A function to plot an image in the fits file'''
-    data = fits.getdata(image)
-    plt.figure(figsize=figsize)
-    plt.imshow(data, vmin=vmin, vmax=vmax, origin='lower', cmap=colourmap)
+import warnings
+warnings.filterwarnings('ignore')
+
+M66_image_path = '/Users/undergradstudent/Research/XRB-Analysis/Galaxies/M66/HST/M66_mosaic_uvis_f555w_drc_sci.fits'
+hdu = fits.open(M66_image_path)
+data = fits.getdata(M66_image_path)
+subset = data[3500:4000, 3500:4000]
+
+def show_galaxy():
+    '''
+    Plot an image of the M66 galaxy.
+    '''
+    plt.figure(figsize=(10, 10))
+    plt.imshow(
+        data, 
+        cmap='viridis',
+        vmin=0, 
+        vmax=0.3,
+        origin='lower',
+        interpolation='nearest'
+    )
+
+def show_zoomed_image(data=subset, vmin=0, vmax=0.5):
+    '''
+    Show a zoomed in image of the M66 galaxy.
+    '''
+    plt.figure(figsize=(10, 10))
+    plt.imshow(data, origin='lower', vmin=vmin, vmax=vmax)
     plt.show()
 
-def show_example_image():
-    '''A function to plot an example image of stars.'''
-    hdu = fits.getdata('M66_hubble.fits')[6000:6500, 2000:2500]
-    plt.figure(figsize=(8, 8))
-    plt.imshow(hdu, origin='lower', vmin=0, vmax=0.3)
-    plt.show()
+def find_stars(
+    size, 
+    sensitivity,
+    data=data,
+    std_multiple=5,
+    sigma=3,
+    plot=True,
+    radius=4,
+    cmap='Greys',
+    vmin=0,
+    vmax=0.3,
+    aperture_color='blue',
+    create_regions=True
+):
+    '''Find all the stars in the M66 galaxy.'''
+    fwhm = {
+        'very low': 0.07,
+        'low': 0.09,
+        'moderately low': 0.02, 
+        'moderate': 0.2,
+        'moderately high': 0.9,
+        'high': 1.5,
+        'very high': 3
+    }
 
+    threshold = {
+        'very low': 0.015,
+        'low': 0.02,
+        'moderately low': 0.08, 
+        'moderate': 0.2,
+        'moderately high': 0.2,
+        'high': 0.5,
+        'very high': 3
+    }
+    mean, median, std = sigma_clipped_stats(data, sigma=sigma)
+    
+    if sensitivity:
+        daofind = DAOStarFinder(fwhm=fwhm[size], threshold=threshold[sensitivity])
+    else:
+        daofind = DAOStarFinder(fwhm=fwhm[size], threshold=5*std)
+    objects = daofind(data)
 
+    print(f"Found {len(objects)} stars")
 
-def find_stars(data, size=3, sensitivity=None, cmap='Greys'):
-    '''A utility function to find stars in an image.'''
-    mean, median, std = sigma_clipped_stats(data, sigma=3.0)
-    if not sensitivity:
-        daofind = DAOStarFinder(fwhm=size, threshold=5.*std)
-    else: daofind = DAOStarFinder(fwhm=size, threshold=sensitivity)
-    sources = daofind(data - median)
-    positions = np.transpose((sources['xcentroid'], sources['ycentroid']))
-    print(f"Your code found {len(positions)} stars...\nLet's plot them!")
-    apertures = CircularAperture(positions, r=4.0)
-    norm = ImageNormalize(stretch=SqrtStretch())
-    plt.figure(figsize=(8, 8))
-    plt.imshow(data, cmap=cmap, origin='lower', norm=norm,
-               interpolation='nearest')
-    apertures.plot(color='blue', lw=1.5, alpha=0.5)
+    positions = np.transpose((objects['xcentroid'], objects['ycentroid']))
 
+    # Create apertures around sources
+    if plot:
+        plt.figure(figsize=(10, 10))
+        apertures = CircularAperture(positions, r=radius)
+        plt.imshow(
+            data,
+            cmap=cmap,
+            vmin=vmin, 
+            vmax=vmax,
+            origin='lower',
+            interpolation='nearest'
+        )
+        apertures.plot(color=aperture_color)
+        plt.show()
+    
+    if create_regions == True:
+        try: wcs = WCS(hdu['SCI'].header)
+        except: wcs = WCS(hdu['PRIMARY'].header)
+        xcoord_img = objects['xcentroid'].tolist()
+        ycoord_img = objects['ycentroid'].tolist()
+        xcoords_fk5, ycoords_fk5 = wcs.wcs_pix2world(xcoord_img, ycoord_img, 1)
+        WriteReg(sources=[xcoords_fk5, ycoords_fk5], coordsys="fk5", \
+                outfile="M66_stars.reg", \
+                radius=0.15, radunit="arcsec", label=objects["id"].tolist(), color='blue')
 
-if __name__ == "__main__":
-    show_example_image()
-    plt.show()
-    data = load_star_image().data[0:401, 0:401]
-    find_stars(data, size=3)
-    plt.show()
+def find_stars_in_zoomed_image(
+    size, 
+    sensitivity,
+    data=subset,
+    std_multiple=5,
+    sigma=3,
+    plot=True,
+    radius=4,
+    cmap='Greys',
+    vmin=0,
+    vmax=0.3,
+    aperture_color='blue'
+):
+    '''Find all the stars in the zoomed in image of the M66 galaxy.'''
+    fwhm = {
+        'very low': 0.07,
+        'low': 0.09,
+        'moderately low': 0.02, 
+        'moderate': 0.2,
+        'moderately high': 0.9,
+        'high': 1.5,
+        'very high': 3
+    }
+
+    threshold = {
+        'very low': 0.015,
+        'low': 0.02,
+        'moderately low': 0.08, 
+        'moderate': 0.2,
+        'moderately high': 0.2,
+        'high': 0.5,
+        'very high': 3
+    }
+    mean, median, std = sigma_clipped_stats(data, sigma=sigma)
+    
+    if sensitivity:
+        daofind = DAOStarFinder(fwhm=fwhm[size], threshold=threshold[sensitivity])
+    else:
+        daofind = DAOStarFinder(fwhm=fwhm[size], threshold=5*std)
+    objects = daofind(data)
+
+    print(f"Found {len(objects)} stars")
+
+    positions = np.transpose((objects['xcentroid'], objects['ycentroid']))
+
+    # Create apertures around sources
+    if plot:
+        plt.figure(figsize=(10, 10))
+        apertures = CircularAperture(positions, r=radius)
+        plt.imshow(
+            data,
+            cmap=cmap,
+            vmin=vmin, 
+            vmax=vmax,
+            origin='lower',
+            interpolation='nearest'
+        )
+        apertures.plot(color=aperture_color)
+        plt.show()
